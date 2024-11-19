fcbd2d192264ca24b17b840a3e1db651a249fe44
kpzip <kpzipgames@gmail.com>
2024-11-19 00:47:26 -0800

    cmd line args

--- iskipper.go
+++ iskipper.go
@@ -3,13 +3,44 @@ package main
 import (
 	"iskipper/iclickerapi"
 	"log"
+	"os"
+	"strings"
 )
 
+const logFileName = "log.txt"
+
 func main() {
+
+	argsWithProg := os.Args
+	argsWithoutProg := argsWithProg[1:]
+	logPath := argsWithoutProg[0] + logFileName
+
+	displayClasses := false
+	var joinCourseId *string = nil
+	retry := false
+	for i, arg := range argsWithProg {
+		if strings.HasPrefix(arg, "-") {
+			if arg == "--courses" {
+				displayClasses = true
+			} else if arg == "--join" {
+				joinCourseId = &argsWithProg[i+1]
+			} else if arg == "--retry" {
+				retry = true
+			} else if strings.Contains(arg, "c") {
+				displayClasses = true
+			} else if strings.Contains(arg, "j") {
+				joinCourseId = &argsWithProg[i+1]
+			} else if strings.Contains(arg, "r") {
+				retry = true
+			}
+		}
+		// ignore if this is part of another argument
+	}
+
 	// get config
 	cfg, err := getConfig()
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalln(err)
 	}
 	// setup client
 	iClickerClient := iclickerapi.Client(cfg.Token, cfg.UserId)
@@ -17,7 +48,7 @@ func main() {
 	if err != nil {
 		log.Fatalln(err)
 	}
-	log.Printf("%+v\n", courses)
+	
 	course := courses[0]
 	attendance, err := iClickerClient.JoinCourseAttendance(course.CourseId, iclickerapi.FromLatLon(34.41135355898463, -119.85483767851561))
 	if err != nil {
